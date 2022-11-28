8cc4d8b59db623b2a86b6322cd779b406810347f
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2022-11-28 19:26:11 +0100

    feat: pre process message

--- sha256_golang/sha256_golang.go
+++ sha256_golang/sha256_golang.go
@@ -1,6 +1,30 @@
 package sha256_golang
 
-import (
-	"errors"
-	"fmt"
-)
+func PreProcess(msg []rune) []rune {
+	proc := msg
+	originalLengthBits := uint64(len(msg) * 8)
+
+	// append 1 (1000 0000)
+	proc = append(proc, 128)
+
+	// pad with zeros such as msg length is a multiple of 512
+	nbZeroBits := (512 + 448 - ((originalLengthBits)%512 + 1)) % 512
+	nbZeroBytes := nbZeroBits / 8
+
+	var i uint64
+	for i = 0; i < nbZeroBytes; i++ {
+		proc = append(proc, 0)
+	}
+
+	// pad original length as 64 bits
+	mask := uint64(0xFF00000000000000)
+
+	for i := 0; i < 8; i++ {
+		val64 := originalLengthBits & mask
+		val := uint8(val64 >> (56 - (8 * i)))
+		proc = append(proc, rune(val))
+		mask >>= 8
+	}
+
+	return proc
+}
--- sha256_golang/sha256_test.go
+++ sha256_golang/sha256_test.go
@@ -1,1 +1,41 @@
 package sha256_golang
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func getShortMessage() string {
+	return "hi"
+}
+
+func getLongMessage() string {
+	return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+}
+
+func TestPreProcessShort(t *testing.T) {
+
+	rawMsg := getShortMessage()
+	msg := PreProcess([]rune(rawMsg))
+
+	pre_processed_bytes := []rune{104, 105, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 16}
+
+	assert.Equal(t, pre_processed_bytes, msg)
+}
+
+func TestPreProcessLong(t *testing.T) {
+
+	rawMsg := getLongMessage()
+	msg := PreProcess([]rune(rawMsg))
+
+	pre_processed_bytes := []rune{97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97,
+		97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97,
+		97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97,
+		97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97,
+		97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 128, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 88}
+
+	assert.Equal(t, pre_processed_bytes, msg)
+}
