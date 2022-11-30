9ddc402b253877f8e81530df7aaf96fc74056b7f
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2022-11-30 13:55:26 +0100

    feat: sha256sum basic usage

--- sha256sum_golang/sha256sum.go
+++ sha256sum_golang/sha256sum.go
@@ -2,9 +2,26 @@ package main
 
 import (
     "fmt"
+    "os"
+
     "github.com/sha256_golang"
 )
 
 func main() {
 
+    if len(os.Args) != 2 {
+        fmt.Println("Usage: sha256_golang <filepath>")
+        return
+    }
+
+    filePath := os.Args[1]
+
+    body, err := os.ReadFile(filePath)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    hash := sha256_golang.Sha256(body)
+    fmt.Printf("%v %v", hash, filePath)
 }
